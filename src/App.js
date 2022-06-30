c6f65868c00af1a6bfce16fb2d1eb041943b2bff
duypham000 <khanhduy.dev@gmail.com>
2022-06-30 19:02:35 +0700

    add testimonial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./scss/_index.scss";
 import "./App.scss";
-import Home from "./components/home";
 
+import Home from "./components/home";
 import About from "./components/about";
 import Portfolio from "./components/portfolio";
 import Photos from "./components/photos";
 import Gallery from "./components/gallery";
+import Testimonials from "./components/testimonials";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Portfolio />
       <Photos />
       <Gallery />
+      <Testimonials />
     </div>
   );
 }
